6cbf40caa82b2d1a6f546be9336c3fc605f9fd70
Joe Denson <joedenson79@gmail.com>
2019-09-19 15:42:41 -0700

    mvp plus 3 stretch goals

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import UserForm from './UserForm';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <UserForm />
+    </div>
+  );
+}
+
+export default App;
